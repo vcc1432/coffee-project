d32d80f4cb3f2ef65f105cacd2efa0193bf4b1ce
vcc1432 <vcc1432@users.noreply.github.com>
2020-07-29 21:21:37 +0000

    deploy: 7f913688272059bd0a905e3aba0eca5808e6e6b9

---
+++ precache-manifest.8b2e97154f71b50a25325f3263129f87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b71346cf5f834fbe340601809de8d276",
+    "url": "/coffee-project/index.html"
+  },
+  {
+    "revision": "037836b063656203d6a7",
+    "url": "/coffee-project/static/css/main.e41ef8f5.chunk.css"
+  },
+  {
+    "revision": "3bf2ed26a39d78a790b3",
+    "url": "/coffee-project/static/js/2.0aaf864b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/coffee-project/static/js/2.0aaf864b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "037836b063656203d6a7",
+    "url": "/coffee-project/static/js/main.e23b9f51.chunk.js"
+  },
+  {
+    "revision": "ce71d09220d214b64470",
+    "url": "/coffee-project/static/js/runtime-main.f447bfe8.js"
+  }
+]);
