188d67d73be0a0ab799b8f557ca17b1c173f93e5
vcc1432 <valerie.gersen@gmail.com>
2020-07-30 00:29:20 +0200

    First attempt modal

--- src/varieties/components/Variety.js
+++ src/varieties/components/Variety.js
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, {
+  useState
+} from 'react';
 import './Variety.css'
 import Card from '../../shared/components/UIElements/Card'
 
 
 const Variety = props => {
+  const [ showDetails, setShowDetails] = useState(false);
+  const openDetailsHandler = () => setShowDetails(true);
+  const closeDetailsHandler = () => setShowDetails(false);
+
+  console.log(showDetails);
+  
   return (
-    <li className="variety-item">
-      <Card className="variety-item__content">
-        <div>
+    <React.Fragment>
+      {showDetails && 
+      <Card className="variety-modal">
+        <div onClick={closeDetailsHandler}>
           <h2>{props.name}</h2>
         </div>
       </Card>
-    </li>
+      }
+      {!showDetails && <li className="variety-item">
+        <Card className="variety-item__content">
+          <div onClick={openDetailsHandler}>
+            <h2>{props.name}</h2>
+          </div>
+        </Card>
+      </li>}
+    </React.Fragment>
   )
 };
 
