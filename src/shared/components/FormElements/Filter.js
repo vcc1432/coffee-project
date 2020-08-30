fa1dc66eb6ab96ef1d73402863b7eecc0b447707
vcc1432 <valerie.gersen@gmail.com>
2020-08-30 22:51:52 +0200

    Upgraded filters with react-select

--- src/shared/components/FormElements/Filter.js
+++ src/shared/components/FormElements/Filter.js
@@ -1,47 +1,41 @@
 import React from 'react';
 
 import './Filter.css';
+import Select from 'react-select';
 
 const Filters = props => {
   return (
-    <div className='filter-wrapper center'>
-    <h1>Filters: </h1>
-      <select multiple className='element filter-select' name='producing_countries' value={props.filters.producing_countries} onChange={props.handleSelect}>
-        <option value='Kenya'>Kenya</option>
-        <option value='Burundi'>Burundi</option>
-        <option value='Colombia'>Colombia</option>
-        <option value='Costa Rica'>Costa Rica</option>
-        <option value='Ethiopia'>Ethiopia</option>
-        <option value='Honduras'>Honduras</option>
-        <option value='Indonesia'>Indonesia</option>
-        <option value='Mexico'>Mexico</option>
-        <option value='Panama'>Panama</option>
-        <option value='Papua New Guinea'>Papua New Guinea</option>
-        <option value='Rwanda'>Rwanda</option>
-        <option value='Tanzania'>Tanzania</option>
-        <option value='Thailand'>Thailand</option>
-        <option value='Vietnam'>Vietnam</option>
-        <option value='Brazil'>Brazil</option>
-        <option value='Peru'>Peru</option>
-      </select>
-
-      <select className='element filter-select' name='quality_potential' value={props.filters.quality_potential} onChange={props.handleSelect}>
-        <option value=''>Search by Quality</option>
-        <option value='EXCEPTIONAL'>Exceptional</option>
-        <option value='VERY_GOOD'>Very good</option>
-        <option value='GOOD'>Good</option>
-        <option value='LOW'>Low</option>
-        <option value='VERY_LOW'>Very low</option>
-      </select>
-
-      <select className='element filter-select' name='leaf_rust' value={props.filters.leaf_rust} onChange={props.handleSelect}>
-        <option value=''>Search by leaf rust resistance</option>    
-        <option value='RESISTANT'>Resistant</option>
-        <option value='TOLERANT'>Tolerant</option>
-        <option value='SUSCEPTIBLE'>Susceptible</option>
-      </select>
-
-      <div className='element clear' onClick={props.handleClearFilters}><p>Clear Filters</p></div>
+    <div className='wrapper'>
+    <h1>Coffee</h1>
+    <div className='filter-wrapper'>
+      <Select
+        className="element"
+        value={props.filters.producing_countries}
+        onChange={props.handleSelect}
+        options={props.options.countryOptions}
+        isMulti
+        placeholder={"Country"}
+        name='producing_countries'
+      />
+      <Select
+        className="element"
+        isClearable
+        value={props.filters.quality_potential}
+        onChange={props.handleSelect}
+        options={props.options.qualityOptions}
+        placeholder={"Quality"}
+        name='quality_potential'
+      />
+      <Select
+        className="element"
+        isClearable
+        value={props.filters.leaf_rust}
+        onChange={props.handleSelect}
+        options={props.options.diseaseOptions}
+        placeholder={"Leaf rust"}
+        name='leaf_rust'
+      />
+      </div>
     </div>
   );
 };
--- src/varieties/pages/Varieties.js
+++ src/varieties/pages/Varieties.js
@@ -17,6 +17,7 @@ const Varieties = () => {
     quality_potential: '',
     leaf_rust: ''
   });
+  const [options, setOptions] = useState();
   
   const {
     isLoading,
@@ -27,6 +28,33 @@ const Varieties = () => {
     const fetchVarieties = async () => {
       try {
         const responseData = await sendRequest('https://coffee-varieties.now.sh/api');
+
+        let rawOptions = responseData.map(({ producing_countries, quality_potential, disease_resistance }) => {
+          let leaf_rust;
+          for (let i = 0; i < disease_resistance.length; i++) {
+            const element = disease_resistance[i];
+            if (element.leaf_rust) {
+              leaf_rust = element.leaf_rust;
+            }
+          }
+          return {producing_countries, quality_potential, leaf_rust }
+        });
+
+        let flatCountriesArray = rawOptions.map(option => option.producing_countries).flat();
+        let qualityArray = rawOptions.map(option => option.quality_potential);
+        let diseaseArray = rawOptions.map(option => option.leaf_rust);
+   
+        let qualityOptions = getOptionsFromArray(qualityArray);
+        let countryOptions = getOptionsFromArray(flatCountriesArray);
+        let diseaseOptions = getOptionsFromArray(diseaseArray);
+
+        let options = {
+          qualityOptions: qualityOptions,
+          countryOptions: countryOptions,
+          diseaseOptions: diseaseOptions
+        }
+        
+        setOptions(options);
         setLoadedVarieties(responseData);
         setVarieties(responseData);
       } catch (err) {}
@@ -34,62 +62,51 @@ const Varieties = () => {
     fetchVarieties();
   }, [sendRequest]);
 
-  const handleSelect = (event) => {
+  const handleSelect = (value, action) => {
     let newFilters;
-    if (event.target.name === 'producing_countries') {
-      let selectedCountries = [];
-      [...event.target.options].filter(option => option.selected).map(option => selectedCountries.push(option.value));
-      newFilters = {
-        ...filters,
-        [event.target.name]: selectedCountries
-      };
-    } else {
-      newFilters = {
-        ...filters,
-        [event.target.name]: event.target.value
-      };
-    }
+    newFilters = {
+      ...filters,
+      [action.name]: value
+    };
 
     setFilters(newFilters);
-
     filterVarieties(newFilters);
     
   };
 
+  const getOptionsFromArray = (optionsArray) => {
+    let result = [...new Set(optionsArray)];
+    let options = result.map(item => {
+      return {value: item, label: item.replace(/_/g, ' ').toLowerCase().split(' ')
+      .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
+      .join(' ')};
+    });
+    return options;
+
+  }
+
   const filterVarieties = (newFilters) => {
     const filteredVarieties = loadedVarieties.filter(item => {
       let quality =
-        newFilters.quality_potential !== "" ?
-        item.quality_potential.toLowerCase() === newFilters.quality_potential.toLowerCase() :
+        newFilters.quality_potential && newFilters.quality_potential !== "" ?
+        item.quality_potential.toLowerCase() === newFilters.quality_potential.value.toLowerCase() :
         true;
       let leafRust =
-        newFilters.leaf_rust !== "" ?
+        newFilters.leaf_rust && newFilters.leaf_rust !== "" ?
         item.disease_resistance.find(disease => {
-          return disease.leaf_rust && (disease.leaf_rust.toLowerCase() === newFilters.leaf_rust.toLowerCase());
+          return disease.leaf_rust && (disease.leaf_rust.toLowerCase() === newFilters.leaf_rust.value.toLowerCase());
         }) :
         true;
       let countries =
-        newFilters.producing_countries.length > 0 ?
-        newFilters.producing_countries.every(country => {
-          return country && item.producing_countries.includes(country);
+        newFilters.producing_countries && newFilters.producing_countries.length > 0 ?
+        newFilters.producing_countries.some(country => {
+          return country && item.producing_countries.includes(country.value);
         }) :
         true;
       return quality && leafRust && countries;
     })
     setVarieties(filteredVarieties);
   }
-
-  const handleClearFilters = () => {
-    let newFilters = {
-      producing_countries: [],
-      quality_potential: '',
-      leaf_rust: ''
-    }
-    setFilters(newFilters);
-
-    filterVarieties(newFilters);
-  }
-
 
   return (
 		<React.Fragment>
@@ -98,10 +115,10 @@ const Varieties = () => {
 					Loading...
 				</div>
 			)}
-			{!isLoading && varieties && <Filters   
+      {!isLoading && varieties && <Filters   
+        options={options}
 				filters={filters}
         handleSelect={handleSelect}
-        handleClearFilters={handleClearFilters}
       />}
       {!isLoading && varieties && <VarietiesList items={varieties} />}
 		</React.Fragment>
