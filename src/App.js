9a931696b7fffece90f927ad430839db96de1e9c
vcc1432 <valerie.gersen@gmail.com>
2020-09-05 16:08:17 +0200

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 import './App.css';
 import Varieties from './varieties/pages/Varieties'
 
 function App() {
-  return ( <
-    Varieties / >
+
+  let routes;
+  return ( 
+    <Router>
+        <main>
+          <Route path="/" exact>
+            <Varieties />
+          </Route>
+          <Route path="/:cid" exact>
+           
+          </Route> 
+          <Redirect to="/" />
+        </main>
+      </Router>
   );
-}
+};
 
 export default App;
--- src/varieties/components/Variety.js
+++ src/varieties/components/Variety.js
@@ -2,7 +2,8 @@ import React, {
   useState
 } from 'react';
 import './Variety.css'
-import Card from '../../shared/components/UIElements/Card'
+import Card from '../../shared/components/UIElements/Card';
+import { NavLink } from 'react-router-dom';
 
 
 const Variety = props => {
@@ -13,16 +14,19 @@ const Variety = props => {
   return (
     <React.Fragment>
     <li className="variety-item">
+   
       <Card className="variety-item__content">
+      <NavLink to={`/${props.name}`}>
         <div onClick={openDetailsHandler}>
           <h2>{props.name}</h2>
           <hr/>
           <p>{props.description}</p>
           <hr/>
           <p>Quality: {props.quality.replace(/_/g, ' ').toLowerCase()}</p>
-
         </div>
+        </NavLink>
       </Card>
+     
     </li>
     </React.Fragment>
   )
